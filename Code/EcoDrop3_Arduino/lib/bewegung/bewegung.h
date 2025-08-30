45ce9b69cd89bb607fb45c4588d1e7bbda44dc04
hizoto <lars.hinder@hotmail.com>
2025-08-30 16:06:33 +0200

    Struktur erweitert

---
+++ Code/EcoDrop3_Arduino/lib/bewegung/bewegung.h
@@ -0,0 +1,19 @@
+#ifndef BEWEGUNG_H
+#define BEWEGUNG_H
+
+class DC_Motor{
+  private:
+    int IN1;
+    int IN2;
+    int ENA;
+  public:
+    DC_Motor(int _IN1, int _IN2, int _ENA);
+
+    void brake();
+
+    void forward(int speed);
+    void backward(int speed);
+};
+
+#endif
+
